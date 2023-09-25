b17ea2451027b37372be4710ed8de047157d4cfa
Casey Deccio <casey@deccio.net>
2023-09-24 22:06:56 -0600

    iterative change

--- 2023-09-25/spin.c
+++ 2023-09-25/spin.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<signal.h>
+#include<sys/types.h>
+
+void somefunc(int signum) {
+	printf("just kidding\n");
+}
 
 /*
  * $ gcc -o spin spin.c
@@ -7,6 +13,14 @@
  * Enter Ctrl-C while program is running
  */
 int main(int argc, char *argv[]) {
+	// setup signal handler
+	struct sigaction sigact;
+	sigact.sa_handler = somefunc;
+	sigact.sa_flags = 0;
+
+	// install signal handler for SIGINT
+	sigaction(SIGINT, &sigact, NULL);
+	
 	for (int i = 0; i < 1000; i++) {
 		printf("main loop %d\n", i);
 		sleep(1);
