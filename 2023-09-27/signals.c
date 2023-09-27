565ce88dd9f4893bb086362327d25149cd6635bf
Casey Deccio <casey@deccio.net>
2023-09-27 11:51:23 -0600

    Iterative change

--- 2023-09-27/signals.c
+++ 2023-09-27/signals.c
@@ -4,6 +4,10 @@
 #include<signal.h>
 #include<sys/types.h>
 
+void somefunc(int signum) {
+	printf("child terminated\n");
+}
+
 /*
  * $ gcc -o signals signals.c
  * $ ./signals
@@ -11,12 +15,21 @@
  */
 int main(int argc, char *argv[]) {
 	pid_t ret = fork();
+	
+	// setup signal handler
+	struct sigaction sigact;
+	sigact.sa_handler = somefunc;
+	sigact.sa_flags = 0;
+
+	// install signal handler for SIGCHLD
+	sigaction(SIGCHLD, &sigact, NULL);
+	
 	if (ret == 0) {
+		sleep(3);
+	} else {
 		for (int i = 0; i < 1000; i++) {
 			printf("main loop %d\n", i);
 			sleep(1);
 		}
 	}
-	sleep(3);
-	kill(ret, SIGINT); // or kill(ret, 2);
 }
