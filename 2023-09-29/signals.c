9a57504936fe1165be89f2269ea0f3f0a3b9b969
Casey Deccio <casey@deccio.net>
2023-09-29 11:14:17 -0600

    Class code

---
+++ 2023-09-29/signals.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<signal.h>
+#include<sys/types.h>
+
+void somefunc(int signum) {
+	for (int i = 0; i < 10; i++) {
+		printf("handler loop %d (signum=%d)\n", i, signum);
+		sleep(1);
+	}
+}
+
+/*
+ * $ gcc -o signals signals.c
+ * $ ./signals
+ * Enter Ctrl-C while program is running
+ * Then again multiple times while handler is running
+ */
+int main(int argc, char *argv[]) {
+
+	// setup signal handler
+	struct sigaction sigact;
+	sigact.sa_handler = somefunc;
+	sigact.sa_flags = 0;
+
+	// install signal handler for SIGINT
+	sigaction(SIGINT, &sigact, NULL);
+	
+	for (int i = 0; i < 1000; i++) {
+		printf("main loop %d\n", i);
+		sleep(1);
+	}
+}
