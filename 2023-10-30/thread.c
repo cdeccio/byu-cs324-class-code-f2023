2aae016e944d12db6cfd3b8e32d353bd39cb8ad3
Casey Deccio <casey@deccio.net>
2023-10-30 11:55:46 -0600

    Iterative change

--- 2023-10-30/thread.c
+++ 2023-10-30/thread.c
@@ -8,16 +8,13 @@
  * $ ./thread
  */
 
-int x = 10;
-
 void *mythreadfunc(void *myargp) {
-	x++;
 	printf("hello from thread\n");
 }
 
 int main() {
 	pthread_t tid;
 	pthread_create(&tid, NULL, mythreadfunc, NULL);
-	sleep(1);
-	printf("x: %d\n", x);
+	sleep(3);
+	printf("main thread terminating\n");
 }
